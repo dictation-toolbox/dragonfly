896c606b01295676acd78d46eeaf442bff7a0a1d
ct.butcher <t4ngo@users.noreply.github.com>
2008-10-18 20:46:51 +0000

    Released version 0.6.1 Modified manifest to exclude .svn subdirectory from documentation output directory.

--- build_docs.py
+++ build_docs.py
@@ -1,12 +1,14 @@
+import sys
 import os
 import subprocess
 
-build_binary = r"c:\python25\scripts\sphinx-build"
+python_binary = sys.executable
+build_binary = r"c:\python25\scripts\sphinx-build-script.py"
 build_type = "html"
 directory = os.path.dirname(__file__)
 src_dir = os.path.abspath(os.path.join(directory, "documentation"))
 dst_dir = os.path.abspath(os.path.join(directory, "dragonfly", "documentation"))
 
-arguments = [build_binary, "-a", "-b", build_type, src_dir, dst_dir]
+arguments = [python_binary, build_binary, "-a", "-b", build_type, src_dir, dst_dir]
 print "Executing:", arguments
 subprocess.call(arguments)
