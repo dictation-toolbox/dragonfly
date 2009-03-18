ae03c1170f3e04b09b1bba6b28de7e14087c79c9
ct.butcher <t4ngo@users.noreply.github.com>
2009-03-18 18:26:10 +0000

    - Included the insert key in the list of special system keys.

--- dragonfly/actions/sendinput.py
+++ dragonfly/actions/sendinput.py
@@ -49,6 +49,7 @@ class KeyboardInput(Structure):
                      win32con.VK_END,
                      win32con.VK_PRIOR,
                      win32con.VK_NEXT,
+                     win32con.VK_INSERT,
                      win32con.VK_LWIN,
                     )
 
