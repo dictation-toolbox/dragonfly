2b381b2a2648b774a62d3c6212103dc28a8ef242
t4ngo <t4ngo@twizzy.biz>
2014-11-01 10:38:09 +0100

    Include words when Mimic action converted to str

--- dragonfly/actions/action_mimic.py
+++ dragonfly/actions/action_mimic.py
@@ -80,6 +80,9 @@ class Mimic(ActionBase):
         if "extra" in kwargs:  self._extra = kwargs.pop("extra")
         else:                  self._extra = None
 
+        # Set pretty printing string used by __str__ and __unicode__.
+        self._str = u", ".join(repr(w) for w in self._words)
+
         # Make sure that all keyword arguments have been consumed.
         if kwargs:
             raise ActionError("Invalid arguments: %r"
