20ad4eee76f54745b3b4529e36322e02320df4d7
Dane Finlay <Danesprite@gmail.com>
2017-08-12 17:54:29 +1000

    Wrote and tested dragonfly2jsgf.py to translate dragonfly grammars and their components into the equivalent JSGF components from the JSGF module

---
+++ jsgf/dragonfly2jsgf.py
@@ -0,0 +1,195 @@
+from dragonfly.grammar.elements import *
+from dragonfly import Grammar
+from dragonfly.parser import Parser, ParserError
+from dragonfly.grammar.elements_compound import stuff
+from dragonfly.grammar.rule_base import Rule
+import jsgf
+from jsgf import Expansion
+import logging
+
+
+class TranslationError(Exception):
+    def __init__(self, msg):
+        super(TranslationError, self).__init__(msg)
+
+
+class LinkedGrammar(jsgf.Grammar):
+    def __init__(self, name, df_grammar):
+        self._df_grammar = df_grammar
+        super(LinkedGrammar, self).__init__(name)
+
+    @property
+    def df_grammar(self):
+        """
+        Dragonfly grammar that this one was built from.
+        :return: Grammar
+        """
+        return self._df_grammar
+
+
+class LinkedRule(jsgf.Rule):
+    def __init__(self, name, visible, expansion, df_rule):
+        self._df_rule = df_rule
+        super(LinkedRule, self).__init__(name, visible, expansion)
+
+    @property
+    def df_rule(self):
+        """
+        Dragonfly rule that this one was built from.
+        :return: Rule
+        """
+        return self._df_rule
+
+
+class TranslationState(object):
+    """
+    Translation state used in building a JSGF expansion tree
+    from a dragonfly rule
+    """
+    def __init__(self, element, expansion=None, dependencies=None):
+        """
+        :type element: ElementBase
+        """
+        self.element = element
+        self.expansion = expansion
+        if isinstance(dependencies, list):
+            self.dependencies = dependencies
+        else:
+            self.dependencies = []
+
+    rule_names = property(
+        lambda self: map(lambda rule: rule.name, self.dependencies)
+    )
+
+
+class Translator(object):
+    _log = logging.getLogger("dragonfly2jsgf")
+    _parser = Parser(stuff, _log)
+
+    def translate(self, df_spec):
+        element = self._parser.parse(df_spec)
+        if not element:
+            raise ParserError("Invalid dragonfly spec: %r" % df_spec)
+
+        assert isinstance(element, ElementBase)
+        state = TranslationState(element)
+        return self.get_jsgf_equiv(state)
+
+    def translate_grammar(self, grammar):
+        """
+        Translate a dragonfly grammar into a JSGF grammar.
+        :type grammar: Grammar
+        :rtype: LinkedGrammar
+        """
+        result = LinkedGrammar(grammar.name, grammar)
+        for rule in grammar.rules:
+            result.add_rule(self.translate_rule(rule))
+        return result
+
+    def translate_rule(self, rule, visible=True):
+        """
+        Translates a dragonfly rule into a JSGF rule.
+        :type rule: Rule
+        :type visible: bool
+        :rtype: LinkedRule
+        """
+        element = rule.element
+        state = self.get_jsgf_equiv(TranslationState(element))
+        jsgf_rule = LinkedRule(rule.name, visible, state.expansion, rule)
+        return jsgf_rule
+
+    def translate_rule_ref(self, state):
+        """
+        Translate the dragonfly RuleRef object inside the TranslationState
+        object by collecting all of the rules as necessary using the
+        'dependencies' list.
+        :type state: TranslationState
+        :return TranslationState
+        """
+        element = state.element
+        if not isinstance(element, RuleRef):
+            raise TranslationError("Cannot translate element '%s' as a "
+                                   "RuleRef." % state.element)
+        name = element.name
+        if name not in state.rule_names:
+            # Make a new equivalent JSGF rule
+            rule_expansion = self.get_jsgf_equiv(element.rule.element)
+            new_rule = jsgf.Rule(name, False, rule_expansion)
+            state.dependencies.append(new_rule)
+            state.expansion = jsgf.RuleRef(new_rule)
+        else:
+            # Use the existing rule
+            index = state.rule_names.index(name)
+            state.expansion = jsgf.RuleRef(state.dependencies[index])
+
+        return state
+
+    def translate_to_rule(self, rule_name, df_spec, visible=True):
+        expansion = self.translate(df_spec).expansion
+        return jsgf.Rule(rule_name, visible, expansion)
+
+    def get_jsgf_equiv(self, state):
+        """
+        Take a TranslationState object containing a dragonfly element
+        return the same object with the 'expansion' member set to the
+        equivalent JSGF expansion object.
+        :type state: TranslationState
+        :return TranslationState
+        """
+        element = state.element
+
+        def get_equiv_children():
+            children = []
+            for child in state.element.children:
+                state.element = child
+                self.get_jsgf_equiv(state)
+                children.append(state.expansion)
+            return children
+
+        if isinstance(element, Literal):  # Literal == word list
+            state.expansion = jsgf.Literal(element.gstring())
+
+        elif isinstance(element, (RuleRef, Rule)):
+            # Translate the dragonfly rule reference
+            self.translate_rule_ref(state)
+
+        elif isinstance(element, (ListRef, List, DictListRef, DictList)):
+            raise NotImplementedError("Dragonfly Lists and DictLists are "
+                                      "not implemented yet.")
+
+        elif isinstance(element, Dictation):
+            # This must be done with a special JSGF tag so a dictation
+            # Sphinx decoder can be prepared and used when necessary.
+            state.expansion = Expansion(())
+            state.expansion.tag = "dictation"
+
+        elif isinstance(element, Impossible):
+            raise TranslationError("Cannot translate 'Impossible' element.")
+
+        elif element.children == ():  # improbable ElementBase case
+            state.expansion = Expansion(())
+
+        elif isinstance(element, Sequence):
+            state.expansion = jsgf.Sequence(*get_equiv_children())
+
+        elif isinstance(element, Alternative):
+            if len(state.element.children) == 1:
+                # Skip redundant (1 child) alternatives
+                state.element = state.element.children[0]
+                self.get_jsgf_equiv(state)
+            else:
+                state.expansion = jsgf.AlternativeSet(*get_equiv_children())
+
+        elif isinstance(element, Optional):
+            equiv_children = get_equiv_children()
+            if len(equiv_children) != 1:
+                raise TranslationError("Optional grouping may only have 1 child.")
+            state.expansion = jsgf.OptionalGrouping(equiv_children[0])
+
+        elif isinstance(element, Repetition):
+            equiv_children = get_equiv_children()
+            if len(equiv_children) != 1:
+                raise TranslationError("Repetition may only have 1 child.")
+            state.expansion = jsgf.Repeat(equiv_children[0])
+
+        return state
---
+++ jsgf/dragonfly2jsgf_test.py
@@ -0,0 +1,94 @@
+import unittest
+
+from dragonfly import CompoundRule
+from jsgf.dragonfly2jsgf import *
+
+
+class TranslatorCase(unittest.TestCase):
+    translator = Translator()
+
+
+class PlainCase(TranslatorCase):
+    def test_plain(self):
+        spec = "Hello world"
+        expected = "public <test> = Hello world;"
+        rule = self.translator.translate_to_rule("test", spec, True)
+        self.assertEqual(expected, rule.compile())
+
+    def test_plain_match(self):
+        spec = "Hello world"
+        rule = self.translator.translate_to_rule("test", spec, True)
+        self.assertTrue(rule.matches("hello world"))
+
+
+class BadRuleCase(TranslatorCase):
+    def test_undefined_rule(self):
+        spec = "Hello <person>"
+        self.assertRaises(Exception, Compound, spec)
+
+    def test_bad_dragonfly_spec(self):
+        def is_bad(spec):
+            self.assertRaises(ParserError, self.translator.translate, spec)
+
+        is_bad("Hello world[")
+        is_bad("Hello world(")
+        is_bad("Hello world)")
+        is_bad("Hello world#")
+        is_bad("Hello world<")
+        is_bad("<[a]>")
+        is_bad("<a>>")
+        is_bad("[]")
+
+
+class OptionalCase(TranslatorCase):
+    def test_simple_optional(self):
+        spec = "Hello [glorious] world"
+        expected = "public <test> = Hello [glorious] world;"
+        self.assertEqual(expected,
+                         self.translator.translate_to_rule("test", spec, True)
+                         .compile())
+
+    def test_complex_optional(self):
+        spec = "[[Hello] world] hi"
+        expected = "public <test> = [[Hello] world] hi;"
+
+        self.assertEqual(expected,
+                         self.translator.translate_to_rule("test", spec, True)
+                         .compile())
+
+
+class ChoiceCase(TranslatorCase):
+    """
+    Test case for testing whether dragonfly choices compile correctly into
+    JSGF
+    """
+    def test_with_choice(self):
+        spec = "Hello <person>"
+        compound_rule = CompoundRule(name="greet", spec=spec, extras=[
+            Choice("person", {"Bob": "Bob", "John": "John"})
+        ])
+        expected = "public <greet> = Hello (Bob|John);"
+        rule = self.translator.translate_rule(compound_rule)
+        self.assertEqual(expected, rule.compile())
+
+    def test_choice_match(self):
+        spec = "Hello <person>"
+        compound_rule = CompoundRule(name="greet", spec=spec, extras=[
+            Choice("person", {"Bob": "Bob", "John": "John"})
+        ])
+        rule = self.translator.translate_rule(compound_rule)
+        self.assertTrue(rule.matches("hello Bob"))
+        self.assertTrue(rule.matches("HELLO BOB"))
+
+
+class GrammarCase(TranslatorCase):
+    def test_simple_rule_grammar(self):
+        grammar = Grammar("test")
+        rule = CompoundRule(name="test", spec="hello world")
+        grammar.add_rule(rule)
+        jsgf_grammar = self.translator.translate_grammar(grammar)
+        self.assertTrue(jsgf_grammar.rules[0].matches("HELLO WORLD"))
+
+
+if __name__ == '__main__':
+    unittest.main()
