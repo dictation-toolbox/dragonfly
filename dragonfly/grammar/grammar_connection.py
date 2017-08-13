61e3407ad30c39436ca2ff06e49d71029995d391
Dane Finlay <Danesprite@gmail.com>
2017-08-14 00:24:03 +1000

    Import mocked Windows-only classes in grammar_connection.py if the platform is non-Windows

--- dragonfly/grammar/grammar_connection.py
+++ dragonfly/grammar/grammar_connection.py
@@ -22,9 +22,17 @@
     This file implements the ConnectionGrammar class.
 """
 
-
-from win32com.client import Dispatch
-from pywintypes import com_error
+try:
+    from win32com.client import Dispatch
+    from pywintypes import com_error
+except ImportError, error:
+    import sys
+    # These modules aren't available on non-Windows platforms, so mock them.
+    if "win" not in sys.platform:
+        from ..os_dependent_mock import Dispatch
+        from ..os_dependent_mock import COMError as com_error
+    else:
+        raise error
 
 from dragonfly.grammar.grammar_base import Grammar
 
