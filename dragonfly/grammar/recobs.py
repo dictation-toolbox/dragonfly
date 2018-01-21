88df357b6c8f119834fc2bd3728f910927d5a0f4
Dane Finlay <Danesprite@gmail.com>
2018-01-21 14:12:16 +1100

    Uncomment some methods in RecognitionObserver base class

--- dragonfly/grammar/recobs.py
+++ dragonfly/grammar/recobs.py
@@ -53,14 +53,14 @@ class RecognitionObserver(object):
         engine = get_engine()
         engine.unregister_recognition_observer(self)
 
-#    def on_begin(self):
-#        pass
-#
-#    def on_recognition(self, words):
-#        pass
-#
-#    def on_failure(self):
-#        pass
+    def on_begin(self):
+        pass
+
+    def on_recognition(self, words):
+        pass
+
+    def on_failure(self):
+        pass
 
 
 #---------------------------------------------------------------------------
