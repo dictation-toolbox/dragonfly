e03004ab40d0958b8d8d20df67622dc3b1433615
Dane Finlay <Danesprite@gmail.com>
2018-02-03 20:17:03 +1100

    Move Sphinx engine GrammarWrapper class to new file

--- dragonfly/engines/backend_sphinx/engine.py
+++ dragonfly/engines/backend_sphinx/engine.py
@@ -21,17 +21,16 @@
 """
 Engine class for CMU Pocket Sphinx
 """
+
 import logging
 import time
-import sys
 
 from jsgf import GrammarError, RootGrammar
 from jsgf.ext import SequenceRule, DictationGrammar, only_dictation_in_expansion
 from pyaudio import PyAudio
 
-import dragonfly.grammar.state as state_
-from dragonfly import Grammar, Window
 from dragonfly.engines.backend_sphinx import is_engine_available
+from dragonfly.engines.backend_sphinx.grammar_wrapper import GrammarWrapper
 from dragonfly2jsgf import Translator, LinkedRule
 from .compiler import SphinxJSGFCompiler
 from .dictation import SphinxDictationContainer
@@ -782,73 +781,3 @@ class SphinxEngine(EngineBase):
         This method should be thread safe.
         """
         self._cancel_recognition_next_time = True
-
-
-class GrammarWrapper(object):
-    def __init__(self, grammar, engine):
-        """
-        :type grammar: Grammar
-        :type engine: SphinxEngine
-        """
-        self.grammar = grammar
-        self.engine = engine
-        self._jsgf_grammar = engine.compiler.compile_grammar(grammar)
-
-    @property
-    def jsgf_grammar(self):
-        return self._jsgf_grammar
-
-    def process_begin(self):
-        """
-        Start the dragonfly grammar processing.
-        """
-        # Get context info for the process_begin method. Dragonfly has a handy
-        # static method for this:
-        fg_window = Window.get_foreground()
-        if sys.platform.startswith("win"):
-            process_method = self.grammar.process_begin
-        else:
-            # Note: get_foreground() is mocked for non-Windows platforms
-            # TODO Change to process_begin once cross platform contexts are working
-            process_method = self.grammar._process_begin
-
-        # Call the process begin method
-        process_method(fg_window.executable, fg_window.title, fg_window.handle)
-
-    def process_results(self, words):
-        """
-        Start the dragonfly processing of the speech hypothesis.
-        :param words: a sequence of (word, rule_id) 2-tuples (pairs)
-        """
-        SphinxEngine._log.debug("Grammar %s: received recognition %r." %
-                                (self.grammar.name, words))
-
-        words_rules = tuple((unicode(w), r) for w, r in words)
-        rule_ids = tuple(r for _, r in words_rules)
-        words = tuple(w for w, r in words_rules)
-
-        # Call the grammar's general process_recognition method, if present.
-        func = getattr(self.grammar, "process_recognition", None)
-        if func:
-            if not func(words):
-                return
-
-        # Iterates through this grammar's rules, attempting to decode each.
-        # If successful, call that rule's method for processing the recognition
-        # and return.
-        s = state_.State(words_rules, rule_ids, self.engine)
-        for r in self.grammar.rules:
-            # TODO Remove the if windows condition when contexts are working
-            if not r.active and sys.platform.startswith("win"):
-                continue
-            s.initialize_decoding()
-
-            # Iterate each result from decoding state 's' with grammar rule 'r'
-            for _ in r.decode(s):
-                if s.finished():
-                    root = s.build_parse_tree()
-                    r.process_recognition(root)
-                    return
-
-        SphinxEngine._log.warning("Grammar %s: failed to decode recognition %r."
-                                  % (self.grammar.name, words))
