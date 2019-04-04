3c13d8241cebe47eef2195d862ee28142e634a88
dwk <dwks42@gmail.com>
2019-04-04 12:25:02 -0400

    Allow voxhub config to be overridden locally.

--- dragonfly/engines/backend_voxhub/config.py
+++ dragonfly/engines/backend_voxhub/config.py
@@ -1,33 +1,6 @@
-# This Python file uses the following encoding: utf-8
-
-"""
-VoxhubEngine engine configuration module.
-"""
-
-# Speech-recognition server (Default: localhost)
-SERVER = "silvius-server.voxhub.io"
-
-# Server port
-PORT = "8019"
-
-# Use the specified content type (default is " + content_type + ")"
-CONTENT_TYPE = "audio/x-raw, layout=(string)interleaved, rate=(int)16000, format=(string)S16LE, channels=(int)1"
-
-# Default path to access in server
-PATH = 'client/ws/speech'
-
-MISC_CONFIG = {
-                'device': -1,                       # Select a different microphone (give device ID)
-                'keep_going': False,                # Keep reconnecting to the server after periods of silence
-                'hypotheses': True,                 # Show partial recognition hypotheses (default: True)
-                'audio_gate': 0,                    # Audio-gate level to reduce detections when not talking
-                'byte_rate': 16000,                 # Rate in bytes/sec at which audio should be sent to the server.
-                'send_adaptation_state': None,      # Send adaptation state from file
-                'save_adaptation_state': None,      # Save adaptation state to file
-                'chunk': 2048 * 2                   # Try adjusting this if you want fewer network packets
-              }
-
-QUIT_PHRASE = "rate"
-SLEEP_PHRASE = "go to sleep"
-WAKE_UP_PHRASE = "wake up"
-
+try:
+    from voxhub_config import *
+    print("Loaded config from local voxhub_config.py")
+except:
+    print("Using default config, create voxhub_config.py locally to override")
+    from default_voxhub_config import *
