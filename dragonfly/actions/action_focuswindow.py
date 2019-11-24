8333efd74917cc84926a1f3301ac5ec9e1d14aa0
David Zurow <daanzu@users.noreply.github.com>
2019-11-23 21:43:40 -0500

    Add FocusWindow options to select by index, or filter by passed func (#173)
    Add FocusWindow options to select by index, or filter by passed func

--- dragonfly/actions/action_focuswindow.py
+++ dragonfly/actions/action_focuswindow.py
@@ -40,34 +40,52 @@ class FocusWindow(ActionBase):
            not case sensitive.
          - *title* (*str*) -- part of the title of the target window;
            not case sensitive.
+         - *index* (*str* or *int*) -- zero-based index of the target
+           window, for multiple matching windows; can be a string (for
+           substitution) but must be convertible to an integer.
+         - *filter_func* (*callable*) -- called with a single argument
+           (the window object), and should return ``True`` for your
+           target windows; example:
+           ``lambda window: window.get_position().dy > 100``.
 
         This action searches all visible windows for a window which
         matches the given parameters.
 
     """
 
-    def __init__(self, executable=None, title=None):
+    def __init__(self, executable=None, title=None, index=None,
+                 filter_func=None):
         if executable:  self.executable = executable.lower()
         else:           self.executable = None
         if title:       self.title = title.lower()
         else:           self.title = None
+        self.index = index
+        self.filter_func = filter_func
         ActionBase.__init__(self)
 
         arguments = []
         if executable:  arguments.append("executable=%r" % executable)
         if title:       arguments.append("title=%r" % title)
+        if index:       arguments.append("index=%r" % index)
+        if filter_func: arguments.append("filter_func=%r" % filter_func)
         self._str = ", ".join(arguments)
 
     def _execute(self, data=None):
         executable = self.executable
         title = self.title
+        index = self.index
         if data and isinstance(data, dict):
             if executable:  executable = (executable % data).lower()
             if title:       title = (title % data).lower()
+            if index:       index = (index % data).lower()
+
+        index = int(index) if index else 0
 
         # Get the first matching window and bring it to the foreground.
         windows = Window.get_matching_windows(executable, title)
-        if windows:
-            windows[0].set_foreground()
+        if self.filter_func:
+            windows = [window for window in windows if self.filter_func(window)]
+        if windows and (index < len(windows)):
+            windows[index].set_foreground()
         else:
             raise ActionError("Failed to find window (%s)." % self._str)
