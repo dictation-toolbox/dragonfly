7ce541daa2c238deffd92556d73988d4017a1b1a
Dane Finlay <Danesprite@posteo.net>
2020-08-28 10:41:27 +1000

    Update Mimic and Playback action documentation to reference quirks
    Closes #270.

--- dragonfly/actions/action_mimic.py
+++ dragonfly/actions/action_mimic.py
@@ -35,6 +35,14 @@ This class could for example be used to open a new Windows Explorer window::
 A more in-depth example is given below in the class reference.
 
 
+Mimic quirks
+----------------------------------------------------------------------------
+
+Some SR engine back-ends have confusing :meth:`engine.mimic` method
+behavior.  See the engine-specific mimic method documentation in sections
+under :ref:`RefEngines` for more information.
+
+
 Class reference
 ----------------------------------------------------------------------------
 
