7f050b6b5cf104bec3f8b70ed45679c7fd3eae62
Dane Finlay <dane@danefinlay.net>
2022-02-26 19:07:21 +1100

    Add xdotool mouse implementation

--- dragonfly/actions/mouse/_xdotool.py
+++ dragonfly/actions/mouse/_xdotool.py
@@ -18,20 +18,76 @@
 #   <http://www.gnu.org/licenses/>.
 #
 
+from __future__                    import print_function
+
+import locale
+from subprocess                    import Popen, PIPE
+import os
+import sys
+
+from six                           import binary_type
+
 from dragonfly.actions.mouse._base import BaseButtonEvent, MoveEvent
+
+#---------------------------------------------------------------------------
+# Helper function for running xdotool commands.
+
+def _run_xdotool_command(arguments):
+    arguments = [str(arg) for arg in arguments]
+    full_command = ["xdotool"] + arguments
+    kwargs = dict(stdout=PIPE, stderr=PIPE)
+
+    # Fork the process with setsid() if on a POSIX system.
+    if os.name == 'posix':
+        kwargs.update(dict(preexec_fn=os.setsid))
+
+    # Execute the command with Popen, logging an error and re-raising
+    # the exception on failure.
+    try:
+        p = Popen(full_command, **kwargs)
+        stdout, stderr = p.communicate()
+    except OSError as exception:
+        raise exception
+
+    # Decode output if it is binary.
+    encoding = locale.getpreferredencoding()
+    if isinstance(stdout, binary_type):
+        stdout = stdout.decode(encoding)
+    if isinstance(stderr, binary_type):
+        stderr = stderr.decode(encoding)
+
+    # Print error messages to stderr. Filter BadWindow messages.
+    stderr = stderr.rstrip()
+    if stderr:
+        print(stderr, file=sys.stderr)
+
+    # Return the process output and return code.
+    return stdout.rstrip(), p.returncode
 
 
 #---------------------------------------------------------------------------
 # Functions and event delegate for getting and setting the cursor position.
 
 def get_cursor_position():
-    # FIXME
-    return x, y
+    # Get the position of the cursor on the screen using xdotool.
+    arguments = ["getmouselocation", "--shell"]
+    stdout, returncode = _run_xdotool_command(arguments)
+    result = None
+    if returncode == 0:
+        lines = stdout.split("\n")
+        assert lines[0].startswith("X=")
+        assert lines[1].startswith("Y=")
+        x = int(lines[0][2:])
+        y = int(lines[1][2:])
+        result = (x, y)
+    return result
 
 
 def set_cursor_position(x, y):
-    # FIXME
-    return xdotool_success
+    # Set the position of the cursor on the screen using xdotool.
+    arguments = ["mousemove", str(x), str(y)]
+    _, returncode = _run_xdotool_command(arguments)
+    return returncode == 0
 
 
 class MoveEventDelegate(object):
@@ -50,44 +106,39 @@ MoveEvent.delegate = MoveEventDelegate
 
 #---------------------------------------------------------------------------
 # xdotool mouse button and wheel up/down flags.
-# FIXME
 
-def get_button(name):
-    # If possible, get a Button from the name.
-    # This allows button and wheel flags to work, at least partially, on
-    # multiple platforms.
-    return getattr(Button, name, None)
+# Note: The "four" and "five" buttons and the left/right scroll events are
+# not supported by xdotool.
 
 
 PLATFORM_BUTTON_FLAGS = {
     # ((button, event_type), down)
     # The inner pair is used here and below to be compatible with the
     # original Windows flags.
-    "left":   (((get_button("left"), 0), 1),  # down
-               ((get_button("left"), 0), 0)),  # up
-    "right":  (((get_button("right"), 0), 1),
-               ((get_button("right"), 0), 0)),
-    "middle": (((get_button("middle"), 0), 1),
-               ((get_button("middle"), 0), 0)),
+    "left":   (((1, 0), 1),  # down
+               ((1, 0), 0)),  # up
+    "right":  (((3, 0), 1),
+               ((3, 0), 0)),
+    "middle": (((2, 0), 1),
+               ((2, 0), 0)),
 
     # We call these "four" and "five" because Windows calls them that.
-    # These buttons typically behave as browser back and forward media keys.
-    "four": (((get_button("button8"), 0), 1),
-             ((get_button("button8"), 0), 0)),
-    "five": (((get_button("button9"), 0), 1),
-             ((get_button("button9"), 0), 0)),
+    "four": ((("four", 0), 1),
+             (("four", 0), 0)),
+    "five": ((("five", 0), 1),
+             (("five", 0), 0)),
 }
 
 PLATFORM_WHEEL_FLAGS = {
     # ((button, event_type), scroll_count)
-    "wheelup": ((get_button("scroll_up"), 1), 3),
-    "stepup": ((get_button("scroll_up"), 1), 1),
-    "wheeldown": ((get_button("scroll_down"), 1), 3),
-    "stepdown": ((get_button("scroll_down"), 1), 1),
-    "wheelright": ((get_button("scroll_right"), 1), 3),
-    "stepright": ((get_button("scroll_right"), 1), 1),
-    "wheelleft": ((get_button("scroll_left"), 1), 3),
-    "stepleft": ((get_button("scroll_left"), 1), 1),
+    "wheelup": ((4, 1), 3),
+    "stepup": ((4, 1), 1),
+    "wheeldown": ((5, 1), 3),
+    "stepdown": ((5, 1), 1),
+    "wheelright": (("wheelright", 1), 3),
+    "stepright": (("stepright", 1), 1),
+    "wheelleft": (("wheelleft", 1), 3),
+    "stepleft": (("stepleft", 1), 1),
 }
 
 
@@ -98,18 +149,30 @@ PLATFORM_WHEEL_FLAGS = {
 class ButtonEvent(BaseButtonEvent):
 
     def execute(self, window):
-        # FIXME
+        # Construct xdotool arguments.
+        arguments = []
         for ((button, event_type), flag) in self._flags:
-            # Check if the button is unknown.
-            pass
-            # if button is None:
-            #     event_type_s = "button" if event_type == 0 else "scroll"
-            #     raise ValueError("Unsupported %s event" % event_type_s)
+            # Raise an error for unsupported buttons.
+            if isinstance(button, str):
+                event_type_s = "button" if event_type == 0 else "scroll"
+                raise ValueError("Unsupported %s event: %s"
+                                 % event_type_s, button)
 
-            # if event_type == 0:  # Button press event
-            #     if flag:
-            #         _controller.press(button)
-            #     else:
-            #         _controller.release(button)
-            # elif event_type == 1:  # Scroll event
-            #     _controller.click(button, flag)
+            # Handle click events.
+            if event_type == 0:
+                command = "mousedown" if flag else "mouseup"
+                arguments.extend([command, str(button)])
+
+            # Handle scroll events.
+            else:
+                arguments.extend([
+                    "click", "--repeat", str(flag), "--delay", "0",
+                    str(button)
+                ])
+
+        # Return early if *arguments* is empty.
+        if len(arguments) == 0:
+            return
+
+        # Send events with xdotool.
+        _run_xdotool_command(arguments)
